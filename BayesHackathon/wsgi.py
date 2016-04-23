db46fc214e7abad34203eccb0cdd7f732e2ad54b
Sushant Wason <sushant@vurb.com>
2016-04-22 23:45:15 -0700

    Updating wsgi

--- BayesHackathon/wsgi.py
+++ BayesHackathon/wsgi.py
@@ -12,11 +12,11 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BayesHackathon.settings")
 
 #application = get_wsgi_application()
 
-#application = Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
 
