ccbfbc946a374cef8202b7d01aa3e0677c4021a1
Sushant Wason <sushant@vurb.com>
2016-04-22 23:39:54 -0700

    Additional Heroku Files

--- BayesHackathon/wsgi.py
+++ BayesHackathon/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BayesHackathon.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+#application = Cling(get_wsgi_application())
+
