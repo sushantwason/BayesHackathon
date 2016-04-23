9a1181d9f0b3a42b86a70b4ba3f11b566e2626ce
Sushant Wason <sushant@vurb.com>
2016-04-23 00:04:43 -0700

    template dir add

--- BayesHackathon/settings.py
+++ BayesHackathon/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(__file__)
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'BayesHackathon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- app/views.py
+++ app/views.py
@@ -9,4 +9,4 @@ from django.views.decorators.csrf import csrf_protect
 # Create your views here.
 
 def index(request):
-    return render_to_response('templates/index.html',context_instance=RequestContext(request))
+    return render_to_response('index.html',context_instance=RequestContext(request))
