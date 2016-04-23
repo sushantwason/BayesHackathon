a7d54af54e886af6e11c01858b3b74dfc3f852d8
Sushant Wason <sushant@vurb.com>
2016-04-22 23:55:26 -0700

    Adding index.html

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django import forms
+from forms import InputForm,SmartsearchForm
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
+
 
 # Create your views here.
+
+def index(request):
+    return render_to_response('index.html',context_instance=RequestContext(request))
