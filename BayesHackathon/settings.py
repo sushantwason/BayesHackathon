681148636a56c610be6284f71e6a81143aa3fe74
Sushant Wason <sushant@vurb.com>
2016-04-23 00:24:39 -0700

    template dir add

--- BayesHackathon/settings.py
+++ BayesHackathon/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'BayesHackathon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "app/templates")],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,10 +69,6 @@ TEMPLATES = [
     },
 ]
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, "app/templates"),
-    # here you can add another templates directory if you wish.
-)
 
 WSGI_APPLICATION = 'BayesHackathon.wsgi.application'
 
